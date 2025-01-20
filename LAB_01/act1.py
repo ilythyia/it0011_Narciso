2f14ea6c279f712ac9627c790837adfa7d5969a4
ilythyia <133185899+ilythyia@users.noreply.github.com>
2025-01-20 10:42:19 +0800

    01/20/2025 1042 - F1-TECHNICAL

--- LAB_01/act1.py
+++ LAB_01/act1.py
@@ -1,2 +1,31 @@
-name = "Narciso"
-print("My name is: ", name)
+print("\n======ENTER YOUR PERSONAL DETAILS======\n")
+
+fname = input("Enter First name: ")
+
+lname = input("Enter Last name: ")
+
+mname = input("Enter Middle name: ")
+
+bday = input("Enter your Birth date: ")
+
+gender = input("Enter your gender (Male/Female): ")
+
+address = input("Enter your Address: ")
+
+course = input("Enter your Course: ")
+
+yearlvl = input("Enter your Year Level: ")
+
+print("\n========================================\n")
+
+
+print("\n=====HERE ARE YOUR PERSONAL DETAILS=====\n")
+print("First Name: ", fname)
+print("Last Name: ", lname)
+print("Middle Name: ", mname)
+print("Birth Date: ", bday)
+print("Gender: ", gender)
+print("Address: ", address)
+print("Course: ", course)
+print("Year Level: ", yearlvl)
+print("\n========================================\n")
