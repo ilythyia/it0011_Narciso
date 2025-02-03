b596f9e512ab57be040b2c512b271ec47ca12a24
ilythyia <133185899+ilythyia@users.noreply.github.com>
2025-02-03 10:34:16 +0800

    ROROROROR

---
+++ TA2/TA2_3.py
@@ -0,0 +1,18 @@
+def file_student_info():
+    """Collects student information and saves it to a file."""
+    
+    f_name = input("Enter your first name: ")
+    l_name = input("Enter your last name: ")
+    age = input("Enter your age : ")
+    co_number = input("Enter contact number: ")
+    course = input("Enter course: ")
+
+    student_info = f"{l_name}, {f_name}, {age}, {co_number}, {course}\n"
+
+    try:
+        with open("students.txt", "a") as file:
+            file.write(student_info)
+        print("Student information has been saved to 'student.txt'. ")
+        
+    except Exception as e: 
+        print(f"An error occured:{e}")
---
+++ TA2/TA2_4.py
@@ -0,0 +1,20 @@
+def show_student_info():
+    """Reads student information from a file and displays it."""
+    
+    try:
+        with open("students.txt", "r") as file:
+            print("Reading Student Information: ")
+        for line in file:
+            l_name, f_name, age, co_number, course = [
+                x.strip() for x in line.strip().split(",")
+            ]
+        
+        print("Last Name: ", l_name)
+        print("First Name: ", f_name)
+        print("Age: ", age)
+        print("Contact Number: ", co_number)
+        print("Course: ", course)
+        print()
+        
+    except Exception as e: 
+        print(f"An error occured:{e}")
